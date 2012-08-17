03565dd2ffcac613d8f464ebc93434ecb9cef7a6
Hieu Pham <mr_hie@yahoo.com>
2012-08-17 15:32:01 +0700

    edit mailer back to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,17 +58,26 @@ Gitlab::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.perform_deliveries = true
-	config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # # config.action_mailer.sendmail_settings = {
+  # #   :location => '/usr/sbin/sendmail',
+  # #   :arguments => '-i -t'
+  # # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  
+	#config.action_mailer.delivery_method = :smtp
+	#config.action_mailer.perform_deliveries = true
+	#config.action_mailer.raise_delivery_errors = true
 
-	config.action_mailer.smtp_settings = {
-	  :address              => "smtp.gmail.com",
-	  :port                 => 587,
-	  :domain               => 'gmail.com',
-	  :user_name            => 'gitlab.lifetimetech@gmail.com',
-	  :password             => 'gitlab123',
-	  :authentication       =>  :plain,
-	  :enable_starttls_auto => true
-	}
+	#config.action_mailer.smtp_settings = {
+	#  :address              => "smtp.gmail.com",
+	#  :port                 => 587,
+	#  :domain               => 'gmail.com',
+	#  :user_name            => 'gitlab.lifetimetech@gmail.com',
+	#  :password             => 'gitlab123',
+	#  :authentication       =>  :plain,
+	#  :enable_starttls_auto => true
+	#}
 end
