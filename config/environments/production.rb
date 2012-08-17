ad5c2dfc987fc23e07b9c201fbc079fe77edec2f
Hieu Pham <mr_hie@yahoo.com>
2012-08-17 11:53:56 +0700

    edit mailer to SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,12 +58,21 @@ Gitlab::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # # config.action_mailer.sendmail_settings = {
-  # #   :location => '/usr/sbin/sendmail',
-  # #   :arguments => '-i -t'
-  # # }
+  # added by hieupv2 for mailer
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+	address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "gitlab.lifetimetech@gmail.com",
+    password: "gitlab123"
+  }
+  # end added by hieupv2
 end
